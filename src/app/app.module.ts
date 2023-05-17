31b8b1e59220a36d08a0fac25dcb475c6988e63a
Romain <Rom.pailhes@gmail.com>
2023-05-17 14:41:22 +0200

    commit before updating angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { NgxIndexedDBModule } from 'ngx-indexed-db';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, NgxIndexedDBModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
